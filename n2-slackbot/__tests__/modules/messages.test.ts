b333d5294b0f2bf8a0d9e9e7ce207277515b2118
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-05-17 17:43:15 +0900

    リファクタリングとUTの追加

---
+++ n2-slackbot/__tests__/modules/messages.test.ts
@@ -0,0 +1,40 @@
+import { initialize, anniversaries } from '../../src/config';
+import { createAnniversaryMessage, createGarbageMessage } from '../../src/modules/messages';
+
+describe('messages.tsのUT', () => {
+  beforeEach(() => {
+    initialize();
+    jest.spyOn(Date, 'now').mockReturnValue(new Date('2020/01/10').getTime());
+  });
+  describe('createAnniversaryMessage()', () => {
+    it('正しいmessageを作成する', () => {
+      const expectMessages = [];
+      expectMessages.push(`${anniversaries[0].text}(${anniversaries[0].date}): 8年1ヶ月25日`);
+      expectMessages.push(`${anniversaries[1].text}(${anniversaries[1].date}): 2年10ヶ月18日`);
+      expectMessages.push(`${anniversaries[2].text}(${anniversaries[2].date}): 2年5ヶ月16日`);
+      expectMessages.push(`${anniversaries[3].text}(${anniversaries[3].date}): 27年0ヶ月0日`);
+      expectMessages.push(`${anniversaries[4].text}(${anniversaries[4].date}): 27年0ヶ月19日`);
+      expectMessages.push(`${anniversaries[5].text}(${anniversaries[5].date}): 4年9ヶ月9日`);
+      expectMessages.push(`${anniversaries[6].text}(${anniversaries[6].date}): 1年4ヶ月13日`);
+
+      const res = createAnniversaryMessage();
+      expect(expectMessages.join(`\n`)).toBe(res);
+    });
+  });
+
+  describe('createGarbageMessage()', () => {
+    it('正しいmessageを作成する', () => {
+      const expectMessages = [];
+      expectMessages.push(`燃やさないゴミは0日後の1/10`);
+      expectMessages.push(`ペットボトルは1日後の1/11`);
+      expectMessages.push(`古紙・ビンカンゴミは12日後の1/22`);
+      expectMessages.push(
+        `https://www.city.arakawa.tokyo.jp/kurashi/gomi/shushubi/syusyubi.files/saisin-gominodasikata.pdf`
+      );
+
+      const res = createGarbageMessage();
+      console.log(res);
+      expect(expectMessages.join(`\n`)).toBe(res);
+    });
+  });
+});
--- n2-slackbot/src/handler.ts
+++ n2-slackbot/src/handler.ts
@@ -1,9 +1,8 @@
 import { postMessage } from './slackBot';
-import lambda from 'aws-lambda';
 import { SlackEvent, Response } from './interface';
-import moment from 'moment';
 import 'moment-precise-range-plugin';
-import { initialize, configInfo, anniversaries } from './config';
+import { initialize, configInfo } from './config';
+import { createAnniversaryMessage, createGarbageMessage } from './modules/messages';
 
 initialize();
 
@@ -40,48 +39,4 @@ function isTargetChannel(slack: SlackEvent): boolean {
     !slack.event.bot_id &&
     (slack.event.channel === configInfo.SLACK_CHANNEL_DEV || slack.event.channel === configInfo.SLACK_CHANNEL)
   );
-}
-
-export function createAnniversaryMessage(): string {
-  return anniversaries
-    .map(anniversary => {
-      const diff = moment.preciseDiff(moment(), moment(anniversary.date, 'YYYY/MM/DD'), true);
-      return `${anniversary.text}(${anniversary.date}): ${diff.years}年${diff.months}ヶ月${diff.days}日`;
-    })
-    .join('\n');
-}
-
-export function createGarbageMessage(): string {
-  const weekdays = ['日', '月', '火', '水', '木', '金', '土'];
-  const garbageWeek = [2, 4];
-  let date = moment().utcOffset('+0900');
-  const message = [];
-  let count = 0;
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const weekCount = Math.floor((date.date() + 6) / 7);
-    const weekday = weekdays[date.days()];
-
-    if (garbageWeek.includes(weekCount)) {
-      if (weekday === '金') {
-        message.push(`燃やさないゴミは${count}日後の${date.format('M/D')}`);
-      }
-      if (weekday === '水') {
-        message.push(`古紙・ビンカンゴミは${count}日後の${date.format('M/D')}`);
-      }
-      if (weekday === '土') {
-        message.push(`ペットボトルは${count}日後の${date.format('M/D')}`);
-      }
-    }
-    if (message.length >= 3) {
-      break;
-    }
-    count++;
-    date = date.add(1, 'd');
-  }
-
-  message.push('https://www.city.arakawa.tokyo.jp/kurashi/gomi/shushubi/syusyubi.files/saisin-gominodasikata.pdf');
-
-  return message.join('\n');
 }
