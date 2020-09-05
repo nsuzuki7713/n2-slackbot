13b04edbd2653c6d06e105c76c1a6a9321a08c74
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-09-06 02:10:34 +0900

    ブランチ差分確認

--- bolt-app/src/modules/releaseNote/commands.ts
+++ bolt-app/src/modules/releaseNote/commands.ts
@@ -1,122 +1,55 @@
 import { app } from '../../app';
+import * as GitHubClient from '../../services/githubClient';
+import { Option } from '@slack/bolt';
 
 export default function () {
   app.command('/release-note', async ({ ack, body, context }) => {
     await ack();
 
     try {
+      const [branches, tags] = await Promise.all([GitHubClient.listBranches(), GitHubClient.listTags()]);
+      const options: Option[] = [...branches, ...tags].map((name) => {
+        return {
+          text: { type: 'plain_text', text: name, emoji: true },
+          value: name,
+        };
+      });
+
       await app.client.views.open({
         token: context.botToken,
-        // 適切な trigger_id を受け取ってから 3 秒以内に渡す
         trigger_id: body.trigger_id,
-        // view の値をペイロードに含む
         view: {
           type: 'modal',
-          callback_id: 'view_1',
-          title: {
-            type: 'plain_text',
-            text: 'リリースノート内容の確認',
-            emoji: true,
-          },
-          submit: {
-            type: 'plain_text',
-            text: '確認',
-            emoji: true,
-          },
-          close: {
-            type: 'plain_text',
-            text: 'Cancel',
-            emoji: true,
-          },
+          callback_id: 'release-note-modal',
+          title: { type: 'plain_text', text: 'リリースノート内容の確認', emoji: true },
+          submit: { type: 'plain_text', text: '確認', emoji: true },
+          close: { type: 'plain_text', text: 'Cancel', emoji: true },
           blocks: [
             {
               type: 'input',
+              block_id: 'base',
               element: {
                 type: 'static_select',
-                placeholder: {
-                  type: 'plain_text',
-                  text: 'master',
-                  emoji: true,
-                },
-                options: [
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '*this is plain_text text*',
-                      emoji: true,
-                    },
-                    value: 'value-0',
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '*this is plain_text text*',
-                      emoji: true,
-                    },
-                    value: 'value-1',
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '*this is plain_text text*',
-                      emoji: true,
-                    },
-                    value: 'value-2',
-                  },
-                ],
+                placeholder: { type: 'plain_text', text: 'master', emoji: true },
+                options,
+                action_id: 'selectedBranch',
               },
-              label: {
-                type: 'plain_text',
-                text: '比較元(Base)ブランチを指定してください。',
-                emoji: true,
-              },
+              label: { type: 'plain_text', text: '比較元(Base)ブランチを指定してください。', emoji: true },
             },
             {
               type: 'input',
+              block_id: 'head',
               element: {
                 type: 'static_select',
-                placeholder: {
-                  type: 'plain_text',
-                  text: 'v1.0.0',
-                  emoji: true,
-                },
-                options: [
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '*this is plain_text text*',
-                      emoji: true,
-                    },
-                    value: 'value-0',
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '*this is plain_text text*',
-                      emoji: true,
-                    },
-                    value: 'value-1',
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '*this is plain_text text*',
-                      emoji: true,
-                    },
-                    value: 'value-2',
-                  },
-                ],
+                placeholder: { type: 'plain_text', text: 'v1.0.0', emoji: true },
+                options,
+                action_id: 'selectedBranch',
               },
-              label: {
-                type: 'plain_text',
-                text: '比較先(compare)のブランチを指定してください。',
-                emoji: true,
-              },
+              label: { type: 'plain_text', text: '比較先(compare)のブランチを指定してください。', emoji: true },
             },
           ],
         },
       });
-      // console.log(result);
     } catch (error) {
       console.error(error);
     }
--- bolt-app/src/modules/releaseNote/views.ts
+++ bolt-app/src/modules/releaseNote/views.ts
@@ -1,12 +1,71 @@
 import { app } from '../../app';
+import * as GitHubClient from '../../services/githubClient';
 
 export default function () {
-  app.view('view_1', async ({ ack, body, view }) => {
-    // モーダルでのデータ送信イベントを確認
+  app.view('release-note-modal', async ({ ack, view, context }) => {
     await ack();
-    // @ts-ignore
-    console.log(view);
-    console.log(body.view.blocks);
-    console.log(body.view.blocks);
+    const base = view['state']['values']['base']['selectedBranch']['selected_option']['value'] as string;
+    const head = view['state']['values']['head']['selectedBranch']['selected_option']['value'] as string;
+
+    const list = await GitHubClient.getPullRequestMergingList(base, head);
+
+    list.map((v) => {
+      return {
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: `${v.message}(<https://github.com/nsuzuki7713/typescript-node-adhoc-scripts/pull/${v.pullRequestNumber}|#${v.pullRequestNumber}>)`,
+          },
+        ],
+      };
+    });
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: 'CJNT26DLJ',
+        text: '',
+        blocks: [
+          {
+            type: 'header',
+            text: { type: 'plain_text', text: ':bulb: 差分情報', emoji: true },
+          },
+          {
+            type: 'context',
+            elements: [{ type: 'plain_text', text: `Baseブランチ: ${base}`, emoji: true }],
+          },
+          {
+            type: 'context',
+            elements: [{ type: 'plain_text', text: `Compareブランチ: ${head}`, emoji: true }],
+          },
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: `<https://github.com/nsuzuki7713/typescript-node-adhoc-scripts/compare/${base}...${head}|GitHubの差分>`,
+              },
+            ],
+          },
+          {
+            type: 'header',
+            text: { type: 'plain_text', text: ':writing_hand: 差分内容', emoji: true },
+          },
+          ...list.map((v) => {
+            return {
+              type: 'context',
+              elements: [
+                {
+                  type: 'mrkdwn',
+                  text: `${v.message}(<https://github.com/nsuzuki7713/typescript-node-adhoc-scripts/pull/${v.pullRequestNumber}|#${v.pullRequestNumber}>)`,
+                },
+              ],
+            };
+          }),
+        ],
+      });
+    } catch (err) {
+      console.log(err);
+    }
   });
 }
